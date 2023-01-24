07ec9d9c300a4b0a8fe808595121f950bf5e29cc
Victoriya Hristova <victoriya.hristova04@gmail.com>
2023-01-24 07:56:35 +0200

    style herobanner

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,22 @@
+import { Box, Stack, Typography, Button } from '@mui/material'
+import React from 'react'
+import HeroBannerImg from '../assets/images/banner.jpg'
+
+function HeroBanner() {
+  return (
+    <Box sx={{
+      mt: {lg: '212px', xs: '70px'},
+      ml: {sm: '50px'}
+    }} position="relative" p="20px">
+      <Typography fontWeight="600" color="#FF2625">Fitness Club</Typography>
+      <Typography fontWeight={700} sx={{
+        fontSize: {lg: '44px', xs: '40px'}
+      }}>Sweat, Smile <br/> and Repeat</Typography>
+      <Typography fontSize="22px" line-height="32px" mb={3}>Check out the most effective exercises</Typography>
+      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
+      <img src={HeroBannerImg} alt="banner" className='hero-banner-img'/>
+    </Box>
+  )
+}
+
+export default HeroBanner
