2c9099609c8f6e749fae055cde4c9e969b197068
Victoriya Hristova <victoriya.hristova04@gmail.com>
2023-01-26 21:10:07 +0200

    connect api

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,16 +1,34 @@
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import { useState } from 'react'
+import { fetchData, exerciseOptions } from '../utils/fetchData'
 
 function SearchExercises() {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async () => {
+    if(search){
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.bodyPart.toLowerCase().includes(search)
+      )
+        console.log(exercisesData);
+    }
+  }
+
   return (
     <Stack alignItems="center" justifyContent="center" mt="37px" p="20px">
       <Typography fontWeight={700} sx={{fontSize: {lg:"44px", xs:"30px"}}} m="50px" textAlign="center">
         Awesome Exercises You Should Know
       </Typography>
       <Box position="relative">
-        <TextField label="Search Exercises" sx={{width: {lg: '700px', xs: '200px'}}}/>
-        <Button variant="contained" color="error" sx={{height:'56px'}}>
+        <TextField value={search} onChange={(e)=> setSearch(e.target.value.toLowerCase())} label="Search Exercises" sx={{width: {lg: '700px', xs: '200px'}}}/>
+        <Button onClick={handleSearch} variant="contained" color="error" sx={{height:'56px'}}>
           Search
         </Button>
       </Box>
